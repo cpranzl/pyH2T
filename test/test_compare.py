ece76ccf6204a11daee1f37e02025d0c2fbc9386
Christoph Pranzl <christoph.pranzl@pranzl.net>
2021-11-05 13:08:42 +0100

    Initial commit.

---
+++ test/test_compare.py
@@ -0,0 +1,11 @@
+def test_greater():
+    num = 101
+    assert num > 100
+
+def test_greater_equal():
+    num = 101
+    assert num >= 100
+
+def test_less():
+    num = 100
+    assert num < 100
---
+++ test/test_square.py
@@ -0,0 +1,10 @@
+import math
+
+def test_sqrt():
+    num = 25
+    assert math.sqrt(num) == 5
+
+def test_power():
+    num = 5
+    exp = 2
+    assert pow(num, exp) == 25
